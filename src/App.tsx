327c2986b56b55d81c59a31f7ad3d2329c429fe1
Agtevoase <Agtevoase@gmail.com>
2021-12-11 17:35:46 +0200

    feat: configured persistor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import './App.css'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import configureStore from 'boot/configureStore'
 
 const App = () => {
-  return <div />
+  const { persistor, store } = configureStore()
+
+  return (
+    <PersistGate persistor={persistor}>
+      <Provider store={store}>
+        <div />
+      </Provider>
+    </PersistGate>
+  )
 }
 
 export default App
