2e9c72ddd78a9fe630a45870f6adeaf39509bb00
Agtevoase <Agtevoase@gmail.com>
2021-12-11 14:43:49 +0200

    fix: updated axios middleware

--- src/services/api/index.tsx
+++ src/services/api/index.tsx
@@ -1,14 +1,28 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 import Routes from 'constants/routes'
-
+import { CaseType, transformProps } from 'services/transformProps'
 
 const instance = axios.create({
   baseURL: `${Routes.API}/api`,
-  timeout: 2000,
+  // timeout: 4000,
   headers: {
-    Authorization: '' // Add bearer token from redux later
-  }
+    Authorization: '', // Add bearer token from redux later
+  },
+  transformRequest: [
+    (data) => transformProps(data, CaseType.SNAKE_CASE),
+    ...((Array.isArray(axios.defaults.transformRequest) ||
+    !axios.defaults.transformRequest
+      ? axios.defaults.transformRequest
+      : [axios.defaults.transformRequest]) || []),
+  ],
+  transformResponse: [
+    ...((Array.isArray(axios.defaults.transformResponse) ||
+    !axios.defaults.transformResponse
+      ? axios.defaults.transformResponse
+      : [axios.defaults.transformResponse]) || []),
+    (data) => (transformProps(data, CaseType.CAMEL_CASE) as AxiosResponse).data,
+  ],
 })
 
 export default instance
