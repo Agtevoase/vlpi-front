d43679bb82c2e5d3eb36e4d2c3cbfb8decbe2b1f
Agtevoase <Agtevoase@gmail.com>
2021-12-12 20:49:11 +0200

    feat: added routes

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,5 +1,11 @@
 enum Routes {
-  API = 'http://194.213.104.146'
+  API = 'http://194.213.104.146',
+  login = '/login',
+  register = '/register',
+  modules = '/modules',
+  exercises = '/exercises',
+  exercise = '/exercises/[id]',
+  profile = '/profile',
 }
 
 export default Routes
