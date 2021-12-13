db6f9eb6115013048afbd0124e64cdfeb0c42ae0
Agtevoase <Agtevoase@gmail.com>
2021-12-13 03:34:47 +0200

    feat: added private routing

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,5 +1,12 @@
-import { BrowserRouter, Route, Routes as Switch } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes as Switch,
+} from 'react-router-dom'
 
+import { ReduxState } from 'store/types'
 import { Routes, getRouterPath } from 'constants/routes'
 
 import LoginPage from './login'
@@ -9,17 +16,35 @@ import ProfilePage from './profile'
 import ExercisePage from './exercises/id'
 import ExercisesPage from './exercises'
 
-const Router: React.FC = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path={Routes.login} element={<LoginPage />} />
-      <Route path={Routes.register} element={<RegisterPage />} />
-      <Route path={Routes.profile} element={<ProfilePage />} />
-      <Route path={getRouterPath(Routes.exercise)} element={<ExercisePage />} />
-      <Route path={Routes.exercises} element={<ExercisesPage />} />
-      <Route path={Routes.home} element={<HomePage />} />
-    </Switch>
-  </BrowserRouter>
-)
+const Router: React.FC = () => {
+  const { token } = useSelector((state: ReduxState) => state.auth)
+
+  const navigateElement = <Navigate to={Routes.login} />
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path={Routes.login} element={<LoginPage />} />
+        <Route path={Routes.register} element={<RegisterPage />} />
+        <Route
+          path={Routes.profile}
+          element={token ? <ProfilePage /> : navigateElement}
+        />
+        <Route
+          path={getRouterPath(Routes.exercise)}
+          element={token ? <ExercisePage /> : navigateElement}
+        />
+        <Route
+          path={Routes.exercises}
+          element={token ? <ExercisesPage /> : navigateElement}
+        />
+        <Route
+          path={Routes.home}
+          element={token ? <HomePage /> : navigateElement}
+        />
+      </Switch>
+    </BrowserRouter>
+  )
+}
 
 export default Router
